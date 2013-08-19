592fad1f3009dff8b6b8766769b78b7ecd147d34
Fredrik Leijon <fredrik.leijon@gmail.com>
2013-08-19 11:08:58 +0200

    initial commit

---
+++ KeepResolution/KeepResolution.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include <Windows.h>
+
+struct res {
+	int bpp, x, y;
+};
+
+void changeResolution(int x, int y, int bpp) {
+	DEVMODE devmode;
+	devmode.dmSize = sizeof(devmode);
+	devmode.dmPelsWidth = x;
+	devmode.dmPelsHeight = y;
+	devmode.dmBitsPerPel = bpp;
+	devmode.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+	auto result  = ChangeDisplaySettings(&devmode, 0);
+}
+
+res getCurrentResolution() {
+	auto hDC = CreateDC(L"DISPLAY", L"", L"", 0);
+
+	auto bpp = GetDeviceCaps(hDC, 12);
+	auto x = GetDeviceCaps(hDC, 8);
+	auto y = GetDeviceCaps(hDC, 10);
+
+	res r = {x, y, bpp};
+	return r;
+}
+
+res getRequestedResolution() {
+	//TODO: Read from config
+	res r = {1920, 1200, 32};
+	return r;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	auto current = getCurrentResolution();
+	auto requested = getRequestedResolution();
+
+	if(current.x != requested.x ||
+		current.y != requested.y ||
+		current.bpp != requested.bpp) {
+		changeResolution(requested.x, requested.y, requested.bpp);
+	}
+
+	return 0;
+}
+
